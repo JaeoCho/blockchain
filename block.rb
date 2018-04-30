b41e706464eb8d275f97303a769f6da8421a58ce
JaeoCho <ksw06137@naver.com>
2018-04-30 20:52:06 +0900

    Block Chain Third

--- block.rb
+++ block.rb
@@ -1,7 +1,27 @@
+require "securerandom"
+
 class BlockChain
 	def initialize
 		@chain = []
+		@transaction = []
+		@wallet = {}
 	end
+
+	def make_a_wallet
+		new_wallet_address = SecureRandom.uuid
+		@wallet[new_wallet_address]=1000
+		@wallet
+	end
+
+	def trans(s,r,a)
+		transaction = {
+			"sender"=>s,
+			"receiver"=>r,
+			"amount"=>a
+		}
+		@transaction << transaction
+	end
+
 	def mining
 		begin
 			nonce=rand(1000000)
@@ -11,8 +31,10 @@ class BlockChain
 			"nonce"=>nonce,
 			"index"=>all_chain.length + 1,
 			"previous_address"=>Digest::SHA256.hexdigest(last_block.to_s),
-			"time"=>Time.now
+			"time"=>Time.now,
+			"transaction"=>@transaction
 		}
+		@transaction=[]
 		@chain<<block
 		nonce
 	end
@@ -20,6 +42,7 @@ class BlockChain
 	def all_chain
 		@chain
 	end
+
 	def last_block
 		@chain[-1]
 	end
--- sinatra_start.rb
+++ sinatra_start.rb
@@ -11,6 +11,7 @@ get '/' do
 		message << "Nonce값 : "+a["nonce"].to_s+"<br>"
 		message << "앞 주소 : "+a["previous_address"].to_s+"<br>"
 		message << "내 주소 : "+Digest::SHA256.hexdigest(a.to_s)+"<br>"
+		message << "거래 : "+a["transaction"].to_s+"<br>"
 		message << "시간 : "+a["time"].to_s+"<br>"
 		message << "<hr>"
 
@@ -21,3 +22,17 @@ end
 get '/mine' do
 	b.mining.to_s		#.to_s: 숫자를 문자로& 시나트라는 꼭 문자로 반환
 end
+
+get '/new_wallet' do
+	b.make_a_wallet.to_s
+end
+
+get '/transaction' do
+#	if params["sender"].nil?
+#		"보내는 사람 정보가 없습니다."
+#	elsif params["receiver"]
+	"보내는 사람 : "+params["sender"] + "받는사람 : "+params["receiver"]+"거래내역 : "+params["amount"]
+	b.trans(params["sender"],params["receiver"],params["amount"])
+	"거래가 완료되었습니다."
+end
+#transcation?sender=a&receiver=b&amount=1.132
