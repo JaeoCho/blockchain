53fb3a4ec8cab29dfa96d99489d09c6c0e6b1dab
JaeoCho <ksw06137@naver.com>
2018-04-30 10:11:04 +0900

    Block Chain Second

--- block.rb
+++ block.rb
@@ -1,9 +1,26 @@
 class BlockChain
+	def initialize
+		@chain = []
+	end
 	def mining
 		begin
-			nonce=rand(100000000)
+			nonce=rand(1000000)
 			hashed = Digest::SHA256.hexdigest(nonce.to_s)
 		end while hashed[0..3] != "0000"
+		block={
+			"nonce"=>nonce,
+			"index"=>all_chain.length + 1,
+			"previous_address"=>Digest::SHA256.hexdigest(last_block.to_s),
+			"time"=>Time.now
+		}
+		@chain<<block
 		nonce
+	end
+
+	def all_chain
+		@chain
+	end
+	def last_block
+		@chain[-1]
 	end
 end
--- sinatra_start.rb
+++ sinatra_start.rb
@@ -5,7 +5,17 @@ require './block'
 b=BlockChain.new
 
 get '/' do
-	"블럭리스트"
+	message=""
+	b.all_chain.each do |a|
+		message << "번호 : "+a["index"].to_s+"<br>"
+		message << "Nonce값 : "+a["nonce"].to_s+"<br>"
+		message << "앞 주소 : "+a["previous_address"].to_s+"<br>"
+		message << "내 주소 : "+Digest::SHA256.hexdigest(a.to_s)+"<br>"
+		message << "시간 : "+a["time"].to_s+"<br>"
+		message << "<hr>"
+
+	end
+	message
 end
 
 get '/mine' do
